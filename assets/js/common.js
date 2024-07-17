75bad913ea1231d9b485cd1930fabf8f0bd158d5
Yunrui Zhang <77179407+Kyrrego@users.noreply.github.com>
2024-07-17 20:36:04 +0800

    Initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,14 @@
+// aHR0cHM6Ly9naXRodWIuY29tL2x1b3N0MjYvYWNhZGVtaWMtaG9tZXBhZ2U=
+$(function () {
+    $('.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        effectTime: 300,
+        visibleOnly: true,
+        placeholder: "",
+        onError: function(element) {
+            console.log('[lazyload] Error loading ' + element.data('src'));
+        }
+    })
+    $('[data-toggle="tooltip"]').tooltip()
+})
